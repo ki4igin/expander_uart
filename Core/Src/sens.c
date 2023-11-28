a1365703e3d503970b24c5717efe4193d7435e18
Emptea <aryas1725@gmail.com>
2023-11-28 19:39:38 +0300

    sens fix

--- Core/Src/sens.c
+++ Core/Src/sens.c
@@ -15,7 +15,7 @@ void adc_sens_data_ready_callback(uint32_t sens_count,
 {
     uint32_t state = 0;
     for (uint32_t i = 0; i < sens_count; i++) {
-        uint32_t is_on = data[i] > SENS_THRESHOLD_12B;
+        uint32_t is_on = data[i] < SENS_THRESHOLD_12B;
         state |= (0x3 * is_on) << (2 * i);
     }
     sens_state = state;
